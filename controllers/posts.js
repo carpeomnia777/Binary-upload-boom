f4426fd051b4b3d1377b6014e518e34cf791b177
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-09-20 10:47:23 -0400

    binary upload boom-added comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -21,7 +22,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments});
     } catch (err) {
       console.log(err);
     }
@@ -73,4 +75,16 @@ module.exports = {
       res.redirect("/profile");
     }
   },
+  createComment: async (req, res) => {
+    try {
+      await Post.create({
+        comment:req.body.id
+      });
+      console.log("comment has been added!");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  
 };
--- models/Post.js
+++ models/Post.js
@@ -20,7 +20,7 @@ const PostSchema = new mongoose.Schema({
   likes: {
     type: Number,
     required: true,
-  },
+  },            
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
